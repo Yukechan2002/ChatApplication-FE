1a1daa1226a53e29d31cf7cbe83eb30a43757ff8
YukeChan2002 <yukeshchandran002@gmail.com>
2024-08-13 22:33:50 +0530

    second comment

--- src/components/specific/ChatList.jsx
+++ src/components/specific/ChatList.jsx
@@ -1,6 +1,6 @@
+import { Stack } from "@mui/material";
 import React from "react";
-import ChatItem from "../shared/ChatItem";
-import "./ChatList.css";
+import ChatItems from "../shared/ChatItems";
 
 const ChatList = ({
   w = "100%",
@@ -16,41 +16,34 @@ const ChatList = ({
   handleDeleteChat,
 }) => {
   return (
-    <div
-      className="container-fluid p-0"
-      style={{ width: w, height: "100%", overflowY: "auto" }}
-    >
-      <ul className="list-group">
-        {chats.map((data, index) => {
-          const { avatar, _id, name, groupChat, members } = data;
+    <Stack width={w} direction={"column"} overflow={"auto"} height={"100%"}>
+      {chats?.map((data, index) => {
+        const { avatar, _id, name, groupChat, members } = data;
 
-          const newMessageAlert = newMessagesAlert.find(
-          
-            ({ chatId }) => chatId === _id
-          );
+        const newMessageAlert = newMessagesAlert.find(
+          ({ chatId }) => chatId === _id
+        );
 
-          const isOnline = members.some((member) =>
-            onlineUsers.includes(member._id)
-          );
+        const isOnline = members?.some((member) =>
+          onlineUsers.includes(member)
+        );
 
-          return (
-            <li key={_id} className="list-group-item p-0">
-              <ChatItem
-                _id={_id}
-                index={index}
-                newMessageAlert={newMessageAlert}
-                isOnline={isOnline}
-                avatar={avatar}
-                name={name}
-                groupChat={groupChat}
-                sameSender={chatId === _id}
-                handleDeletedChat={handleDeleteChat}
-              />
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+        return (
+          <ChatItems
+            index={index}
+            newMessageAlert={newMessageAlert}
+            isOnline={isOnline}
+            avatar={avatar}
+            name={name}
+            _id={_id}
+            key={_id}
+            groupChat={groupChat}
+            sameSender={chatId === _id}
+            handleDeleteChat={handleDeleteChat}
+          />
+        );
+      })}
+    </Stack>
   );
 };
 
